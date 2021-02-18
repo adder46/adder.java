931a56923b54dc1e9e7d2edb6ae0b67b02288516
adder46 <dedmauz69@gmail.com>
2021-02-18 10:09:56 +0100

    Improve comments

--- lib/src/main/java/com/github/adder46/libadder/RippleCarryAdder.java
+++ lib/src/main/java/com/github/adder46/libadder/RippleCarryAdder.java
@@ -12,27 +12,25 @@ public class RippleCarryAdder {
 
     public int add(int a, int b) throws AdditionOverflowException {
         for (int i = 0; i < adders.length; i++) {
-            /*  We are extracting the bits in reverse, by shifting right `i` positions,
-             *  where 0 <= i < 8. In the beginning, i=0, so we are not shifting anything.
-             *  However, each time through the loop, the number of positions (`i`) we are
-             *  shifting right increases by 1 until it reaches 7. This means that the
-             *  previously extracted bit is discarded, and the bit we want to extract next
-             *  comes to its place, i.e., to the least significant position. By Boolean ANDing
-             *  the resulting number with 1 (mask), the individual bit is effectively extracted
-             *  from the number - masking out the rest.
+            /* The bits are extracted from the narrow end by shifting right `i` positions,
+             * where 0 <= i < adders.length. On first iteration, i == 0, meaning that nothing is shifted.
+             * However, each time through the loop, the number of shifted positions (`i`)
+             * increases by 1 until it reaches adders.length - 1. As a result, previously extracted bit is
+             * discarded and the bit we want to extract next comes to its place, i.e., becomes LSB.
+             * By bitwise ANDing the resulting number with 1 (mask), the individual bit at the end
+             * is effectively extracted from the number - masking out the rest.
              */
             int bit1 = (a >> i) & 1;
             int bit2 = (b >> i) & 1;
             adders[i].add(bit1, bit2);
             try {
-                // pass the carry bit if there is an adder following the current one
+                // Pass the carry bit to the next adder if the current adder is not the last one.
                 adders[i + 1].setCarryIn(adders[i].getCarryOut());
             } catch (ArrayIndexOutOfBoundsException e) {
                 if (adders[adders.length - 1].getCarryOut() == 1) {
-                    /*  having no adders that come after the current one,
-                     *  means that current adder is the last one, and if
-                     *  the carry out for that adder is 1, 8-bit ripple carry
-                     *  adder overflowed.
+                    /* Having no adders that come after the current one,
+                     * means that current adder is the last one. Furthermore,
+                     * if the carry out for that adder is 1, we have an overflow.
                      */
                     throw new AdditionOverflowException("Overflow.");
                 }
@@ -42,12 +40,11 @@ public class RippleCarryAdder {
     }
 
     private int getResult() {
-        /*  Since the adding process was in reverse,
-         *  when building the result, the adders are
-         *  reversed so that the adder holding the MSB
-         *  comes first. We start from 0, and pack the bits
-         *  back into the result, by shifting left by 1
-         *  each time through the loop and ORing with the sum.
+        /* Since the addition was done starting from the narrow end,
+         * when building the result, the adders are reversed so that
+         * the adder holding the MSB comes first. The bits are packed
+         * back into the result by shifting left by 1 each time through
+         * the loop and ORing the result with the sum of an individual adder.
          */
         int result = 0;
         for (int i = adders.length - 1; i >= 0; i--) {
